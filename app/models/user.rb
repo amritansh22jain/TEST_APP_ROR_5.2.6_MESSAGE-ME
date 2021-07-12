0ae563dac65cbd1629c22c6ff266648701b37026
Amritansh Jain <amitansh.jain@gmail.com>
2021-07-12 22:37:14 +0530

    Added Messages Resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximmum: 15}
   validates_uniqueness_of :username, case_sensitive: false
+  has_many :messages
   has_secure_password
 end
